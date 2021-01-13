ad9d918e88a685565f6c48fbf1219a9f8b778155
Liar-paradox <konrad.zupanski@gmail.com>
2021-01-13 11:26:31 +0100

    lab10

---
+++ lab 10 zad 1.py
@@ -0,0 +1,21 @@
+n =(''' Magia jest w opinii niektórych ucieleśnieniem Chaosu. Jest kluczem zdolnym otworzyć
+        zakazane drzwi. Drzwi, za którymi czai się koszmar, zgroza i niewyobrażalna okropność,
+        za którymi czyhają wrogie, destrukcyjne siły, moce czystego zła, mogące unicestwić nie
+        tylko tego, kto drzwi te uchyli, ale i cały świat. A ponieważ nie brakuje takich, którzy przy
+        owych drzwiach manipulują, kiedyś ktoś popełni błąd, a wówczas zagłada świata będzie
+        przesądzona i nieuchronna. Magia jest zatem zemstą i orężem Chaosu. To, że po
+        Koniunkcji Sfer ludzie nauczyli posługiwać się magią, jest przekleństwem i zgubą świata.
+        Zgubą ludzkości. I tak jest. Ci, którzy uważają magię za Chaos, nie mylą się.''')
+print(n.lower())
+print(n.swapcase())
+print(n.capitalize())
+print(n.replace('Magia','XD'))
+print(n.lstrip())
+print(n.rstrip())
+print(n.__reversed__())
+print(n.count('jest'))
+print(n.find('jest'))
+print(n.isalnum())
+print(n.startswith())
+print(n.startswith())
+print(n.endswith())
---
+++ lab 10 zad 3.py
@@ -0,0 +1,5 @@
+tekst = f"""Długo na szturm i szaniec poglądał w milczeniu. Na koniec rzekł: 'Stracona'."""
+
+tekst1 = tekst[:(tekst.find(".", 0,len(tekst)-1))]
+
+print(tekst1)
