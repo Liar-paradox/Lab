c65e271257e0c416acc0c7af8426c4393b4fa40f
Liar-paradox <konrad.zupanski@gmail.com>
2021-04-30 14:57:25 +0200

    :D

--- Laby 2/Lab 15/zad 5.py
+++ Laby 2/Lab 15/zad 5.py
@@ -5,12 +5,14 @@ class Hotel:
         self.pietra = pietra
         self.liczba_pokoi = liczba_pokoi
         self.liczba_pieter = []
+        self.pokoje = []
 
 
 
         for x in range(self.pietra):
             self.liczba_pieter.append([])
 
+        self.utworz()
 
     def utworz(self):
         print(self.liczba_pieter)
@@ -18,7 +20,44 @@ class Hotel:
             liczba_pok_piet = random.randint(1, self.liczba_pokoi - self.pietra)
             for x in range(liczba_pok_piet):
                 self.liczba_pieter[y].append(x)
+                self.pokoje.append(x)
         print(self.liczba_pieter)
+
+    def wolne_pok(self):
+        counter = 0
+        for x in self.pokoje:
+            if x.zajety == False:
+                counter += 1
+        print('Jest {} wolnych pokoi'.format(counter))
+
+    def wynajecie(self,osoba):
+        for x in self.pokoje:
+            if x.zajety == False:
+                x.zajety = True
+                x.lokator = osoba #zamiast osoba to Osoba(osoba.imie)
+
+    def znajdz(self,osoba):
+        for x in self.pokoje:
+            if x.lokator.imie == osoba.imie:
+                print('Osoba {} wynajmuje pokoj'.format(osoba))
+
+class Pokoj:
+    def __init__(self,nr,pietro):
+        self.nr = nr
+        self.pietro = pietro
+        self.zajety = False
+        self.lokator = None
+
+    def __repr__(self):
+        return 'Pokoj {} znajduje sie na pietrze {}'.format(self.nr,self.pietro)
+
+class Osoba:
+    def __init__(self, imie):
+        self.imie = imie
+
+    def __repr__(self):
+        return '{}'.format(self.imie)
+
 
 
 
